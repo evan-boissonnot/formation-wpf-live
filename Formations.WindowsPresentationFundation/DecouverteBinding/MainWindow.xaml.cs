7bdd7e4d3c62a9f7a58f073f529bce3ee22ab6d6
evan-boissonnot <commercial@boissonnot.Fr>
2019-01-10 21:37:46 +0100

    presentation bindind, datatemplate, connection base de données

---
+++ Formations.WindowsPresentationFundation/DecouverteBinding/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecouverteBinding
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.UpdateSizeOfTextBox();
+        }
+
+        private void txtTailleFont_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.UpdateSizeOfTextBox();
+        }
+
+        private void UpdateSizeOfTextBox()
+        {
+            this.UpdateSizeOfTextBox(this.lblInfo, this.txtTailleFont);
+        }
+
+        private void UpdateSizeOfTextBox(Label label, TextBox textBox)
+        {
+            int size = 0;
+
+            int.TryParse(this.txtTailleFont.Text, out size);
+            label.FontSize = size == 0 ? 50 : size;
+        }
+    }
+}
---
+++ Formations.WindowsPresentationFundation/TravailWpfAvecEntities/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravailWpfAvecEntities
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Initialize();
+        }
+
+        private void Initialize()
+        {
+            using (HarryPotter.Core.Data.HarryPotterEntities context = new HarryPotter.Core.Data.HarryPotterEntities())
+            {
+                var query = from wiwi in context.Witch
+                            select wiwi;
+
+                var maList = query.ToList();
+
+                this.lstWitches.ItemsSource = maList;
+            }
+        }
+    }
+}
